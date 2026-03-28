f8aa00e70cd6d159edb18e51e1f80c8b7670213f
geo14150 <paulos.giorgos1996@gmail.com>
2026-03-28 20:30:22 +0200

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/movie_app/',   
+  base: '/movie_app/',  
 })
